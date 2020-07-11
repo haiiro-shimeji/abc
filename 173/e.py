798cefd51d78fed8c25be98cf1f36f7f794654d0
ikeda.shoichi <ikeda.shoichi@gmail.com>
2020-07-11 20:58:14 +0900

    Update 173-e

--- 173/e.py
+++ 173/e.py
@@ -20,26 +20,51 @@ def readIntegerSet(n):
 def readIntegerMatrix(n, m):
     return reduce(lambda acc, _: acc + [readIntegerSet(m)], range(0, n), [])
 
+def last(a, op):
+    for i in range(len(a)-1, -1):
+        if op(a[i]):
+            return i
+    return -1
+
+def first(a, op):
+    for i in range(0, len(a)):
+        if op(a[i]):
+            return i
+    return -1
+
 def main(N, K, A):
-    A = [(abs(a), a > 0) for a in A]
+    A = [(abs(a), a, a > 0) for a in A]
     A.sort(reverse=True, key=lambda _t: _t[0])
 
     candidate = A[0:K]
 
     # if candidate is negative
-    if len(list(filter(lambda _t: not _t[1], candidate)))%2:
-        if candidate[K-1] < 0:
-            candidate.append(A[K][0])
-        else:
-            if A[K+1][0] < 0:
-                candidate.append(A[K+1][0])
+    if len(list(filter(lambda _t: not _t[2], candidate)))%2:
+        # 候補の最小負値を残りの最大正値で代える
+        i10 = last(candidate, lambda _t: not _t[2])
+        i11 = first(A[K:], lambda _t: _t[2])
+        impact1 = - i11 / i10 if i10 != -1 and i11 != -1 else 0
+
+        # 候補の最小正値を残りの最大負値で代える
+        i20 = last(candidate, lambda _t: _t[2])
+        i21 = first(A[K:], lambda _t: not _t[2])
+        impact2 = - i21 / i20 if i20 != -1 and i21 != -1 else 0
+
+        if 0 < impact1 or 0 < impact2:
+            # おおきくなれる方を採用
+            if impact1 > impact2:
+                candidate[i10] = A[K+i11]
             else:
-                candidate[K-2] = A[K][0]
-                candidate.append(A[K+1][0])
+                candidate[i20] = A[K+i21]
+        else:
+            # 負値を解消するすべがないので、仕方なく最大の負値を採る.
+            candidate = A[N-K:]
 
     result = 1
-    for a in candidate:
-        result = (result * a) % 1000000007
+    for _t in candidate:
+        result = result * _t[1]
+        if result > 1000000006 or result < 0:
+            result %= 1000000007
         
     return result
 
