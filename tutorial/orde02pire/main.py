4551c1670f4f4946e177ccb6d4cd58b007465ee1
Shoichi Ikeda <ikeda@localhost.localdomain>
2021-01-12 13:00:26 +0900

    Speed up a little.

--- tutorial/orde02pire/main.py
+++ tutorial/orde02pire/main.py
@@ -35,19 +35,35 @@ def _coords(code):
 
     return (_coord(code[0]), _coord(code[1]))
 
-def _closed_ranges(a):
-    sorted_a = sorted(a)
-    for i in range(len(sorted_a)):
-        for j in range(i, len(sorted_a)):
-            yield (sorted_a[i], sorted_a[j]+1)
+def _fetch_closed_rect(dots):
+    def _closed_ranges(a):
+        sorted_a = sorted(a)
+        for i in range(len(sorted_a)):
+            for j in range(i, len(sorted_a)):
+                yield (sorted_a[i], sorted_a[j]+1)
 
-def _opened_ranges(a):
-    sorted_a = sorted(a)
-    sorted_a = [-1] + sorted_a + [62]
+    x_ranges = list(_closed_ranges([x for x, y in dots]))
+    y_ranges = list(_closed_ranges([y for x, y in dots]))
 
-    for i in range(len(sorted_a)):
-        for j in range(i+1, len(sorted_a)):
-            yield (sorted_a[i]+1, sorted_a[j])
+    for x0, x1 in x_ranges:
+        for y0, y1 in y_ranges:
+            yield x0, x1, y0, y1
+
+def _fetch_opened_rect(dots):
+    def _opened_ranges(a):
+        sorted_a = sorted(a)
+        sorted_a = [-1] + sorted_a + [62]
+
+        for i in range(len(sorted_a)):
+            for j in range(i+1, len(sorted_a)):
+                yield (sorted_a[i]+1, sorted_a[j])
+
+    x_ranges = list(_opened_ranges([x for x, y in dots]))
+    y_ranges = list(_opened_ranges([y for x, y in dots]))
+
+    for x0, x1 in x_ranges:
+        for y0, y1 in y_ranges:
+            yield x0, x1, y0, y1
 
 def _main(_S):
     N = int(_S.split(':')[0])
@@ -55,37 +71,39 @@ def _main(_S):
 
     logging.debug([N, dots])
 
-    min_value = 62 * 62 + 1
-    max_value = -1
+    min_value = None
+    max_value = None
 
     # 最小
-    x_ranges = list(_closed_ranges([x for x, y in dots]))
-    y_ranges = list(_closed_ranges([y for x, y in dots]))
-    for x0, x1 in x_ranges:
-        for y0, y1 in y_ranges:
-            # この四角に入っている点の数
-            n = len([(x, y) for x, y in dots if x0 <= x and x < x1 and y0 <= y and y < y1])
-            if N == n:
-                value = (x1 - x0) * (y1 - y0)
-                if value < min_value:
-                    logging.debug('min cand: {}: {}'.format(n, [x0, x1, y0, y1]))
-                    min_value = value
+    rects = sorted(
+        [((x0, x1, y0, y1) ,(x1 - x0) * (y1 - y0)) for x0, x1, y0, y1 in _fetch_closed_rect(dots)],
+        key=lambda t: t[1],
+        reverse=False
+    )
+    for coords, S in rects:
+        x0, x1, y0, y1 = coords
+        # この四角に入っている点の数
+        n = len([(x, y) for x, y in dots if x0 <= x and x < x1 and y0 <= y and y < y1])
+        if N == n:
+            min_value = S
+            break
 
     # 最大
-    x_ranges = list(_opened_ranges([x for x, y in dots]))
-    y_ranges = list(_opened_ranges([y for x, y in dots]))
-    for x0, x1 in x_ranges:
-        for y0, y1 in y_ranges:
-            # この四角に入っている点の数
-            n = len([(x, y) for x, y in dots if x0 <= x and x < x1 and y0 <= y and y < y1])
-            if N == n:
-                value = (x1 - x0) * (y1 - y0)
-                if value > max_value:
-                    logging.debug('max cand: {}: {}'.format(n, [x0, x1, y0, y1]))
-                    max_value = value
+    rects = sorted(
+        [((x0, x1, y0, y1) ,(x1 - x0) * (y1 - y0)) for x0, x1, y0, y1 in _fetch_opened_rect(dots)],
+        key=lambda t: t[1],
+        reverse=True
+    )
+    for coords, S in rects:
+        x0, x1, y0, y1 = coords
+        # この四角に入っている点の数
+        n = len([(x, y) for x, y in dots if x0 <= x and x < x1 and y0 <= y and y < y1])
+        if N == n:
+            max_value = S
+            break
 
     return '{},{}'.format(min_value, max_value) \
-            if min_value < 62 * 62 + 1 and max_value > -1 else '-'
+            if min_value is not None and max_value is not None else '-'
 
 if __name__ == "__main__":
     print(_main(readString()))
